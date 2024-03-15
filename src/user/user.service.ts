7d65df5c24bb558f4d3e979c66d95425e6d8d1bf
강대현 <motojate@naver.com>
2024-03-15 17:28:36 +0900

    [FEAT] 회원가입 로직에 비밀번호 암호화 로직 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,14 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/public/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Prisma, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
+  private async generateHashedPassword(password: string) {
+    const saltRound = 10;
+    const salt = await bcrypt.genSalt(saltRound);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  }
+
   async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { email, password } = createUserDto;
+    const hashedPassword = await this.generateHashedPassword(password);
     return this.prisma.user.create({
-      data: createUserDto,
+      data: {
+        email: email,
+        password: hashedPassword,
+      },
     });
   }
 
